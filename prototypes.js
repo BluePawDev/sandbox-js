eae2f2edbc60a7d6c39e183470ed25df0b87d069
BluePawDev <jaruby@me.com>
2017-05-05 10:47:11 -0500

    Test Drill() constructor

--- prototypes.js
+++ prototypes.js
@@ -1,11 +1,23 @@
-console.log("source is good!");
-
-var dogHouse = {
-  location: "backyard",
-  heightInFeet: 2
-};
-
-dogHouse.color = [];
 
 
-console.log(dogHouse);
+// START Drill constructor
+function Drill(inUse, isCharged, bitType, bitSize, rotation){
+  this.statusUse = inUse;
+  this.statusCharge = isCharged;
+  this.bitType = bitType;
+  this.bitSize = bitSize;
+  this.rotation = rotation;
+  this.status = function () {
+    if (statusCharged === false){
+      console.log("The drill needs to be charged");
+    } else if (statusInUse === true) {
+      console.log("The drill is charged, but someone is using it with a " + bitSize + bitType + " bit");
+    } else {
+      console.log("The drill is charged and available--have fun!");
+    }
+  };
+} // END Drill constructor
+
+var myDrill = new Drill(true, true, "masonry", 0.375, "clockwise");
+
+console.log(myDrill);
